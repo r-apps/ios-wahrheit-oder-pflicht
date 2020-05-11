ddb57e935092706645038dd1d9c431ac73663cd9
Ruben Vitt <dev@rubeen.dev>
2020-05-11 14:25:11 +0200

    some additions

--- Wahrheit oder Pflicht/ContentView.swift
+++ Wahrheit oder Pflicht/ContentView.swift
@@ -10,7 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Image(systemName: "moon")
+                
+            
+            HStack {
+                Button(action: {
+                    
+                }) {
+                    Text("Wahrheit")
+                }
+                
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Pflicht")
+                }
+            }
+        }
     }
 }
 
